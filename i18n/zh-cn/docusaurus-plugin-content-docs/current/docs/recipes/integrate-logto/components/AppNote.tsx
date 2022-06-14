8bbe86325d278ed2ebf63d9a2835012ad75b5d8e
Gao Sun <gao@silverhand.io>
2022-06-14 21:31:58 +0800

    chore: add eslint script

--- i18n/zh-cn/docusaurus-plugin-content-docs/current/docs/recipes/integrate-logto/components/AppNote.tsx
+++ i18n/zh-cn/docusaurus-plugin-content-docs/current/docs/recipes/integrate-logto/components/AppNote.tsx
@@ -1,5 +1,5 @@
-import React, { ReactNode } from 'react'
 import Admonition from '@theme/Admonition';
+import React, { ReactNode } from 'react';
 
 type Props = {
   type: ReactNode;
@@ -8,9 +8,10 @@ type Props = {
 const AppNote = ({ type }: Props) => {
   return (
     <Admonition type="note">
-      本篇教程默认你已经在管理界面中成功创建了一个 {type} 应用。如果你还没有完成这一步操作，请参阅这篇教程，创建应用之后再继续。
+      本篇教程默认你已经在管理界面中成功创建了一个 {type}{' '}
+      应用。如果你还没有完成这一步操作，请参阅这篇教程，创建应用之后再继续。
     </Admonition>
   );
 };
 
-export default AppNote
+export default AppNote;
--- src/theme/Footer/index.tsx
+++ src/theme/Footer/index.tsx
@@ -1,25 +1,27 @@
-import React from 'react';
-import {useThemeConfig} from '@docusaurus/theme-common';
+import { useThemeConfig } from '@docusaurus/theme-common';
+import FooterCopyright from '@theme/Footer/Copyright';
 import FooterLinks from '@theme/Footer/Links';
 import FooterLogo from '@theme/Footer/Logo';
-import FooterCopyright from '@theme/Footer/Copyright';
+import React from 'react';
+
 import FooterLayout from './FooterLayout';
 
-
 const Footer = () => {
-  const {footer} = useThemeConfig();
+  const { footer } = useThemeConfig();
+
   if (!footer) {
     return null;
   }
-  const {copyright, links, logo, style} = footer;
+  const { copyright, links, logo, style } = footer;
+
   return (
     <FooterLayout
       style={style}
-      links={links && links.length > 0 && <FooterLinks links={links} />}
+      links={links.length > 0 && <FooterLinks links={links} />}
       logo={logo && <FooterLogo logo={logo} />}
       copyright={copyright && <FooterCopyright copyright={copyright} />}
     />
   );
-}
+};
 
 export default React.memo(Footer);
