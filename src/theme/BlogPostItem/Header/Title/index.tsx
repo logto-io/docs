2aafe7453c010ed73fd1374af31f7659c54f69a4
Gao Sun <gao@silverhand.io>
2023-05-24 11:27:45 +0800

    refactor: use a tender approach for redirecting to the new blog

--- src/theme/BlogPostItem/Header/Title/index.tsx
+++ src/theme/BlogPostItem/Header/Title/index.tsx
@@ -1,8 +1,9 @@
 import Link from '@docusaurus/Link';
 import { useBlogPost } from '@docusaurus/theme-common/internal';
+import Admonition from '@theme/Admonition';
 import type { Props } from '@theme/BlogPostItem/Header/Title';
 import clsx from 'clsx';
-import { useEffect } from 'react';
+import { useCallback, useEffect } from 'react';
 
 import styles from './styles.module.css';
 
@@ -10,13 +11,9 @@ const Title = ({ className }: Props): JSX.Element => {
   const { metadata, isBlogPostPage } = useBlogPost();
   const { permalink, title } = metadata;
   const TitleHeading = isBlogPostPage ? 'h1' : 'h2';
+  const shouldReplace = window.location.pathname.startsWith('/blog');
 
-  // Redirect to the new blog. The whole component will be removed later.
-  useEffect(() => {
-    if (!window.location.pathname.startsWith('/blog')) {
-      return;
-    }
-
+  const getNewBlogUrl = useCallback(() => {
     const url = new URL(
       isBlogPostPage ? window.location.pathname.replace(/^\/blog/, '') : '/',
       'https://blog.logto.io'
@@ -27,23 +24,55 @@ const Title = ({ className }: Props): JSX.Element => {
       url.searchParams.append(key, value);
     }
 
-    // eslint-disable-next-line @silverhand/fp/no-mutation
-    window.location.href = url.href;
+    return url;
   }, [isBlogPostPage]);
 
+  // Redirect to the new blog. The whole component will be removed later.
+  useEffect(() => {
+    if (!shouldReplace) {
+      return;
+    }
+
+    // https://developers.google.com/search/docs/crawling-indexing/javascript/javascript-seo-basics#properly-inject-canonical-links
+    // Remove all canonical tags before inserting
+    for (const element of document.head.querySelectorAll('link')) {
+      if (['canonical', 'alternate'].includes(element.getAttribute('rel') ?? '')) {
+        element.parentNode?.removeChild(element);
+      }
+    }
+
+    // Insert the canonical tag to the new blog
+    const linkTag = document.createElement('link');
+    linkTag.setAttribute('rel', 'canonical');
+    // eslint-disable-next-line @silverhand/fp/no-mutation
+    linkTag.href = getNewBlogUrl().href + '/';
+    document.head.append(linkTag);
+  }, [getNewBlogUrl, shouldReplace]);
+
   return (
-    <TitleHeading
-      className={clsx(styles.title, !isBlogPostPage && styles.listTitle, className)}
-      itemProp="headline"
-    >
-      {isBlogPostPage ? (
-        title
-      ) : (
-        <Link itemProp="url" to={permalink}>
-          {title}
-        </Link>
+    <>
+      {shouldReplace && (
+        <>
+          <Admonition type="tip">
+            Logto Blog has been moved to the new place!{' '}
+            <a href={getNewBlogUrl().href}>Click here</a> to see this article in the new blog.
+          </Admonition>
+          <hr></hr>
+        </>
       )}
-    </TitleHeading>
+      <TitleHeading
+        className={clsx(styles.title, !isBlogPostPage && styles.listTitle, className)}
+        itemProp="headline"
+      >
+        {isBlogPostPage ? (
+          title
+        ) : (
+          <Link itemProp="url" to={permalink}>
+            {title}
+          </Link>
+        )}
+      </TitleHeading>
+    </>
   );
 };
 
