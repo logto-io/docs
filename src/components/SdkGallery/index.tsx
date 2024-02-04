a70d8f9c9805a4d83d09585574582c5387bace0a
Gao Sun <gao@silverhand.io>
2024-02-04 17:00:21 +0800

    feat: add sdk gallery

---
+++ src/components/SdkGallery/index.tsx
@@ -0,0 +1,95 @@
+import { useDocsSidebar } from '@docusaurus/theme-common/internal';
+
+import fallbackImage from './fallback.png';
+import styles from './index.module.scss';
+
+type Category = {
+  label: string;
+  description?: string;
+  items: Array<{ label: string; href: string }>;
+};
+
+const sdkPath = '/sdk/';
+
+/**
+ * A component that shows the gallery of SDKs.
+ *
+ * Note: This component is only available in the "SDK" tab of the documentation.
+ */
+const SdkGallery = () => {
+  const sidebar = useDocsSidebar();
+
+  if (!sidebar) {
+    return null;
+  }
+
+  const sections = sidebar.items.reduce<Category[]>((data, item) => {
+    if (item.type !== 'link') {
+      return data;
+    }
+
+    if (item.unlisted || item.href === sdkPath) {
+      return data;
+    }
+
+    if (item.href === '#') {
+      const description = item.customProps?.description;
+
+      return [
+        ...data,
+        {
+          label: item.label,
+          description: description ? String(description) : undefined,
+          items: [],
+        },
+      ];
+    }
+
+    if (!item.href.startsWith(sdkPath)) {
+      return data;
+    }
+
+    const lastCategory = data[data.length - 1];
+
+    if (!lastCategory) {
+      return data;
+    }
+
+    return [
+      ...data.slice(0, -1),
+      {
+        ...lastCategory,
+        items: [...lastCategory.items, { label: item.label, href: item.href }],
+      },
+    ];
+  }, []);
+
+  return (
+    <section className={styles.gallery}>
+      {sections.map((category) => (
+        <div key={category.label}>
+          <h2>{category.label}</h2>
+          {category.description && <p className={styles.description}>{category.description}</p>}
+          <section className={styles.list}>
+            {category.items.map((item) => (
+              <a key={item.href} href={item.href} className={styles.link}>
+                <img
+                  className={styles.logo}
+                  title="Logo"
+                  src={`/img/logo/${item.href.slice(sdkPath.length)}.png`}
+                  onError={({ currentTarget }) => {
+                    // eslint-disable-next-line @silverhand/fp/no-mutation
+                    currentTarget.src = fallbackImage;
+                  }}
+                />
+                <span>{item.label}</span>
+              </a>
+            ))}
+          </section>
+        </div>
+      ))}
+    </section>
+  );
+};
+
+export default SdkGallery;
