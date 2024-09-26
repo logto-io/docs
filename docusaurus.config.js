10099ab591d52d35903a0a95fe3a15d115094011
Gao Sun <gao@silverhand.io>
2024-09-27 00:47:11 +0800

    refactor: update clarity id (#783)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -81,7 +81,7 @@ const injectHeadTagsPlugin = () => ({
                 c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
                 t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
                 y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "o96ufwq738");
+            })(window, document, "clarity", "script", "o9cb961ir0");
           }
         `,
       },
