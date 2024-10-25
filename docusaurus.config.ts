16868946f6fb7918eb1e34bedce450c594c7ed83
Charles Zhao <charleszhao@silverhand.io>
2024-10-25 15:22:14 +0800

    fix: disable breadcrumb

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -125,7 +125,7 @@ const config: Config = {
       {
         docs: {
           routeBasePath: '/',
-          breadcrumbs: true,
+          breadcrumbs: false,
           sidebarPath: './src/sidebars.ts',
           editUrl: 'https://github.com/logto-io/docs/tree/master',
           editLocalizedFiles: true,
