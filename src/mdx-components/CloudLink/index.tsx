1fefb092bbc084e94ee1d73200c2998581e53792
Charles Zhao <charleszhao@silverhand.io>
2024-11-12 11:27:05 +0800

    feat: add cloud console link component (#804)

---
+++ src/mdx-components/CloudLink/index.tsx
@@ -0,0 +1,53 @@
+import { joinPath } from '@silverhand/essentials';
+import { type PropsWithChildren } from 'react';
+
+type Props = PropsWithChildren<{
+  /**
+   * The path to navigate to in the Cloud console. E.g., `to="applications"`.
+   * If the path is an absolute URL, it will be used as is.
+   */
+  readonly to: string;
+}>;
+
+const logtoCloudConsoleUrl = 'https://cloud.logto.io';
+const logtoCloudTenantIdWildcard = 'to';
+
+const isAbsoluteUrl = (url: string) => {
+  return url.startsWith('http://') || url.startsWith('https://');
+};
+
+const getCloudConsoleUrl = (path: string) => {
+  if (isAbsoluteUrl(path)) {
+    return path;
+  }
+
+  const baseUrl = new URL(logtoCloudConsoleUrl);
+  const pathname =
+    path.startsWith(`/${logtoCloudTenantIdWildcard}/`) ||
+    path.startsWith(`${logtoCloudTenantIdWildcard}/`)
+      ? path
+      : joinPath(logtoCloudTenantIdWildcard, path);
+
+  return new URL(pathname, baseUrl).toString();
+};
+
+/**
+ * Remove the possible leading slashes and capitalize the first letter
+ */
+const toTitleCaseLabel = (path: string) => {
+  const pathWithoutLeadingSlashes = path.replace(/^\/+/, '');
+  // Capitalize the first letter
+  return pathWithoutLeadingSlashes.charAt(0).toUpperCase() + pathWithoutLeadingSlashes.slice(1);
+};
+
+const CloudLink = ({ to, children }: Props) => {
+  const link = getCloudConsoleUrl(to);
+
+  return (
+    <a href={link} target="_blank" rel="noopener">
+      {children || toTitleCaseLabel(to)}
+    </a>
+  );
+};
+
+export default CloudLink;
