4b738d9c922ef0980189fd9edf168cb40a8b0c11
Charles Zhao <charleszhao@silverhand.io>
2024-11-14 11:14:14 +0800

    feat: add new url component (#806)

---
+++ src/components/Url/index.tsx
@@ -0,0 +1,46 @@
+import Link from '@docusaurus/Link';
+import isInternalUrl from '@docusaurus/isInternalUrl';
+import ExternalLinkIcon from '@theme/Icon/ExternalLink';
+import clsx from 'clsx';
+import { type ComponentProps } from 'react';
+
+import ApiIcon from '@site/src/assets/api.svg';
+import DocumentIcon from '@site/src/assets/document.svg';
+import VideoIcon from '@site/src/assets/video.svg';
+
+import styles from './index.module.scss';
+
+const isApiDocLink = (href?: string) =>
+  href?.startsWith('https://bump.sh/logto/') || href?.startsWith('https://openapi.logto.io/');
+
+const isVideoLink = (href?: string) =>
+  href?.startsWith('https://www.youtube.com/') || href?.startsWith('https://youtu.be/');
+
+const isDocLink = (href?: string) =>
+  href?.startsWith('https://blog.logto.io/') ||
+  href?.startsWith('https://auth.wiki/') ||
+  isInternalUrl(href);
+
+export type Props = ComponentProps<'a'> & {
+  readonly hasIcon?: boolean;
+  readonly type?: 'block' | 'inline';
+};
+
+const Url = (props: Props): JSX.Element => {
+  const { className, children, hasIcon = true, type = 'block', ...rest } = props;
+  const isInternal = isInternalUrl(props.href);
+
+  return (
+    <span className={clsx(styles.linkWrapper, type === 'inline' && styles.inline)}>
+      <Link className={clsx(className, styles.link)} {...rest}>
+        {hasIcon && isApiDocLink(props.href) && <ApiIcon className={styles.flexWidth} />}
+        {hasIcon && isVideoLink(props.href) && <VideoIcon />}
+        {hasIcon && isDocLink(props.href) && <DocumentIcon />}
+        {children}
+        {!isInternal && <ExternalLinkIcon className={styles.externalLink} />}
+      </Link>
+    </span>
+  );
+};
+
+export default Url;
--- src/theme/MDXComponents/index.ts
+++ src/theme/MDXComponents/index.ts
@@ -1,10 +1,12 @@
 import MDXComponents from '@theme-original/MDXComponents';
 
 import CloudLink from '@site/src/components/CloudLink';
+import Url from '@site/src/components/Url';
 
 const components = {
   ...MDXComponents,
   CloudLink,
+  Url,
 };
 
 export default components;
