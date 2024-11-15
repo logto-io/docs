6a6b461b73e91113c773e4e385d255cf458d5371
Charles Zhao <charleszhao@silverhand.io>
2024-11-15 18:13:05 +0800

    docs: add index doc for introduction

--- src/theme/MDXComponents/index.ts
+++ src/theme/MDXComponents/index.ts
@@ -4,9 +4,12 @@ import CloudLink from '@site/src/components/CloudLink';
 import NavGroup from '@site/src/components/NavGroup';
 import Url from '@site/src/components/Url';
 
+import DocCardList from '../DocCardList';
+
 const components = {
   ...MDXComponents,
   CloudLink,
+  DocCardList,
   NavGroup,
   Url,
 };
