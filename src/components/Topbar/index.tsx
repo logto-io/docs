c311147c8b46407e2c5d73ab72b5ca98d421b759
Gao Sun <gao@silverhand.io>
2024-11-29 12:09:31 +0800

    refactor: improve gallery i18n (#893)

--- src/components/Topbar/index.tsx
+++ src/components/Topbar/index.tsx
@@ -1,15 +1,18 @@
 import { useColorMode } from '@docusaurus/theme-common';
 
+import { useCurrentLocalePrefix } from '@site/src/hooks/useCurrentLocalePrefix';
+
 import styles from './index.module.scss';
 
 const Topbar = () => {
   const { colorMode } = useColorMode();
+  const prefix = useCurrentLocalePrefix();
 
   return (
     <div className={styles.section}>
       <div className={styles.container}>
         <div className="col col--offset-1">
-          <a href="https://logto.io">
+          <a href={new URL(prefix, 'https://logto.io').href}>
             <img
               src={`/img/logto${colorMode === 'dark' ? '_dark' : ''}.svg`}
               alt="Logto logo"
