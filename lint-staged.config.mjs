13aad106bd978c2d6e2d74f0cf71b021413255da
Charles Zhao <charleszhao@silverhand.io>
2025-06-13 22:49:29 +0800

    chore: update lint-stage config

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,5 +1,4 @@
 const configs = {
-  'tutorial/**/!(generated-|_template-)*.md?(x)': ['pnpm lint --fix'],
   '(src|docs|i18n)/**/*.md?(x)': ['pnpm lint --fix'],
   '!(*.config).ts?(x)': ['pnpm lint --fix', () => 'tsc -p tsconfig.json --noEmit'],
 };
