b028178e389acbc7ca4946b6caea84857ffe212c
Darcy Ye <darcyye@silverhand.io>
2026-02-25 11:30:42 +0800

    feat(docs): add env switch to disable Google One Tap script loading (#1364)
    * feat(docs): add env switch to disable Google One Tap script loading
    
    - add GOOGLE_ONE_TAP_SCRIPT_ENABLED to control GSI script injection
    - skip loading https://accounts.google.com/gsi/client when disabled

--- docusaurus-common.config.ts
+++ docusaurus-common.config.ts
@@ -349,13 +349,16 @@ export const classicPresetConfig = {
 };
 
 // Google One Tap related configurations
-export const googleOneTapScripts = [
-  {
-    src: 'https://accounts.google.com/gsi/client',
-    async: true,
-    defer: true,
-  },
-];
+const isGoogleOneTapScriptEnabled = yes(process.env.GOOGLE_ONE_TAP_SCRIPT_ENABLED);
+export const googleOneTapScripts = isGoogleOneTapScriptEnabled
+  ? [
+      {
+        src: 'https://accounts.google.com/gsi/client',
+        async: true,
+        defer: true,
+      },
+    ]
+  : [];
 
 export const createGoogleOneTapCustomFields = () => ({
   isProduction: yes(process.env.IS_PRODUCTION),
